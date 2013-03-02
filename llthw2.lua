d48a87f4c263713ba745f8ab852df6031a25dab2
Gary Katsevman <git@gkatsev.com>
2013-03-02 16:27:38 -0500

    finish chapter2

---
+++ llthw2.lua
@@ -0,0 +1,14 @@
+-- Here's a piece of comment that describes what the program does.
+-- The following line of code prints out some text
+print("Some random text...")
+-- Two dashes found in a print text are not comments
+print("-- This will still print")
+
+print("there's a comment after this") -- Comments can also come after the code
+
+-- In fact, everything that comes after -- in code not inside of text are comments
+
+
+print('print(-- "Hello there")') -- this doesn't work because the second ) is commented out
+
+-- `print -- ("hello world")` doesn't work because it needs to be called.
