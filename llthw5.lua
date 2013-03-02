02310a1147df17aac12af81d109ba402e63f5fdc
Gary Katsevman <git@gkatsev.com>
2013-03-02 16:55:16 -0500

    finish chapter 5

--- llthw5.lua
+++ llthw5.lua
@@ -7,3 +7,17 @@ print(text)
 
 print(string.sub(text, 2))
 print(string.sub(text, 1, 10))
+
+print(string.byte("a"))
+print(string.find(text, "e"))
+print(#text)
+print(string.len(text))
+print(string.lower(text))
+print(string.rep(text, 2))
+print(string.sub(text,3,3))
+
+print("asdf" == "asdf")
+print("ASDF" == "asdf")
+print("asdf" > "asdf")
+print("asdfb" > "bsdf")
+print("a" < "b")
