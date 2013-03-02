e763d9f795dd3545ed0fcadee9bd716542c495c9
Gary Katsevman <git@gkatsev.com>
2013-03-02 16:40:32 -0500

    finish chapter 4

--- llthw4.lua
+++ llthw4.lua
@@ -14,3 +14,12 @@ distance = 100 -- km
 time = 1 -- hour
 velocity = distance/time
 print("the car is traveling at a velocity of", velocity) -- but where is the unit?
+
+_ = 3
+__ = 3
+-- for = 3
+print("for isn't valid", _, __, "but _ and __ are")
+c = c
+print(c)
+print(C)
+print(A)
