99c8cc31b6ac53649b6dedafe17090de13708ca7
Gary Katsevman <git@gkatsev.com>
2013-03-02 17:12:54 -0500

    chapter 6

---
+++ llthw6.lua
@@ -0,0 +1,19 @@
+cat1 = 10
+cat2 = 11
+cat3 = 12
+cat4 = 8
+cat5 = 100 -- LONG CAT IS LONG
+
+print("average length of the cats is", (cat1+cat2+cat3+cat4+cat5)/5)
+
+cats = {10, 11, 12, 8, 100}
+cat1 = cats[1]
+cat3 = cats[3]
+print("average length of the cats is", (cats[1]+cats[2]+cats[3]+cats[4]+cats[5])/5)
+
+cats = {10, 11, 12, 8, 100, 28}
+print("average length of the cats is", (cats[1]+cats[2]+cats[3]+cats[4]+cats[5]+cats[6])/#cats)
+
+cats = {{"bob", 10}, {"LONGCAT", 100}}
+print(cats[1][1], cats[1][2])
+print(cats[2][1], cats[2][2])
